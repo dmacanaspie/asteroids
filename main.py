6661c61e378ba342f1577ca28dac84ac0e8e9a56
dmacanaspie <duane.macanaspie@gmail.com>
2025-06-21 16:37:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import (
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    ASTEROID_KINDS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_SPAWN_RATE
+)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            else:
+                pygame.Surface.fill(screen, (0, 0, 0))
+                pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()

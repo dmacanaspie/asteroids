ede49b19dbe642535d1dc98ba1892fdef0966879
dmacanaspie <duane.macanaspie@gmail.com>
2025-06-21 21:35:48 -0400

    updated with use of containers and groups

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0  # delta time
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
@@ -27,8 +30,9 @@ def main():
                 run = False
             else:
                 pygame.Surface.fill(screen, (0, 0, 0))
-                player.update(dt)
-                player.draw(screen=screen)
+                updatable.update(dt)
+                for item in drawable:
+                    item.draw(screen=screen)
                 pygame.display.flip()
 
         dt = clock.tick(60) / 1000

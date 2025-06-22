b30e8c041a724c97c930e8c9c248da3f6a4fb4f8
dmacanaspie <duane.macanaspie@gmail.com>
2025-06-21 22:11:32 -0400

    added asteroid and asteroid field

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import (
     SCREEN_HEIGHT,
     SCREEN_WIDTH,
@@ -18,10 +20,18 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # add containers to class prior to creating variables
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
+    asteroidField = AsteroidField()
 
     run = True
     while run:
